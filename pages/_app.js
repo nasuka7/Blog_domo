53c391c76855a6a4975de9032c2cd270ca8708fe
nasuka7 <n4suka7@gmail.com>
2021-03-21 12:12:13 +0900

    Add nav and about page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Link from 'next/link';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link hlef="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link hlef="/">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          my blog
+        </h1>        
+      </main>
+    </div>
+  )
+}
